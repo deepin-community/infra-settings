e901d15eba39d0b04a091401ebad19bd8f94c0b1
hudeng <hudeng@deepin.org>
2023-04-03 13:58:32 +0800

    feat: obs api add proxy support
    目前obs api服务跑到容器里面还存在问题,先使用代理的方式添加线上obs api服务的网络带宽

--- services/obs/obsapi/apply.sh
+++ services/obs/obsapi/apply.sh
@@ -8,3 +8,9 @@ openssl x509 -req -days 365 -in config/certs/server.csr \
         -signkey config/certs/server.key -out config/certs/server.crt
 
 sudo docker build -t hub.deepin.com/k3s/obs/apiv2
+
+
+kubectl create namespace obs-api
+kubectl apply -f obs-api-proxy.yml -n obs-api
+# edit obs nginx config file
+#kubectl edit cm obsapi-nginx-config
