53d87aaa7804c5bae94f6de44bbf7283e860f921
hudeng <hudeng@deepin.org>
2023-04-16 16:52:50 +0800

    feat(CI): kilo network web tools add deepinid support
    support login by deepinid and use rbac manager permission

--- services/kilo-network/apply.sh
+++ services/kilo-network/apply.sh
@@ -2,5 +2,10 @@
 
 kubectl apply -f crds.yaml
 kubectl apply -f kilo-k3s.yaml
-#web service
-kubectl apply -f kilo-wg-gen-web.yaml
+#web service, support fo oauth2, will be used by deepinid
+kubectl apply -f kilo-wg-gen-web-oidc-rbac.yaml
+
+#kubectl -n default create secret generic kilo-wg-gen-web \
+#    --from-literal=cookie-secret="" \
+#    --from-literal=client-id="" \
+#    --from-literal=client-secret=""
