6b3ab94ae589319969415a4b91c0959b61a24d34
hudeng <hudeng@deepin.org>
2023-05-09 17:24:32 +0800

    chore: obs api service add topic support patch

--- services/obs/obsapi/entrypoint.sh
+++ services/obs/obsapi/entrypoint.sh
@@ -8,4 +8,5 @@ apache2ctl -D FOREGROUND &
 # for github proxy
 echo "127.0.0.1       github.com" >> /etc/hosts
 echo "127.0.0.1       api.github.com" >> /etc/hosts
+echo "127.0.0.1       raw.githubusercontent.com" >> /etc/hosts
 /usr/local/bin/gost -L "sni://127.0.0.1:443?limiter.in=5MB&limiter.out=5MB" -F "wss://${IP}:443?host=${sstring}"
