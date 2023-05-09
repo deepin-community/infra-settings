79654a7df15dab458076845fd2d24aa5eb494452
hudeng <hudeng@deepin.org>
2023-05-09 09:50:06 +0800

    chore: obs api server remove ssl cert support
    instead of using k3s cluster cert manager

--- services/obs/obsapi/apply.sh
+++ services/obs/obsapi/apply.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #generate server key files 
-openssl genrsa -out config/certs/server.key 1024
-openssl req -new -key config/certs/server.key \
-        -out config/certs/server.csr
-openssl x509 -req -days 365 -in config/certs/server.csr \
-        -signkey config/certs/server.key -out config/certs/server.crt
+#openssl genrsa -out config/certs/server.key 1024
+#openssl req -new -key config/certs/server.key \
+#        -out config/certs/server.csr
+#openssl x509 -req -days 365 -in config/certs/server.csr \
+#        -signkey config/certs/server.key -out config/certs/server.crt
 
 sudo docker build -t hub.deepin.com/k3s/obs/apiv2
 
 
 kubectl create namespace obs-api
-kubectl apply -f obs-api-proxy.yml -n obs-api
+#kubectl apply -f obs-api-proxy.yml -n obs-api
 # edit obs nginx config file
 #kubectl edit cm obsapi-nginx-config
