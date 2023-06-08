73723996e1b40e11b1b95a94c9402ae53a9bb709
hudeng <hudeng@deepin.org>
2023-06-08 14:09:24 +0800

    obs: api service add search and delay job support

--- services/obs/obsapi/entrypoint.sh
+++ services/obs/obsapi/entrypoint.sh
@@ -4,6 +4,10 @@ chmod 777 -R /srv/www/obs/api/log
 # /usr/bin/memcached -u memcached -l 127.0.0.1 &
 # sleep 20
 apache2ctl -D FOREGROUND &
+export RAILS_ENV=production
+sudo -E -u wwwrun /usr/bin/bundle.ruby3.1 exec /srv/www/obs/api/script/delayed_job.api.rb --queue=default start -i 1030 &
+sudo -E -u wwwrun /usr/bin/bundle.ruby3.1 exec script/delayed_job.api.rb --queue=scm start -i 1070 &
+sudo -E -u wwwrun /srv/www/obs/api/bin/rails sphinx:start &
 
 # for github proxy
 echo "127.0.0.1       github.com" >> /etc/hosts
