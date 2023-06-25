f9b6e69bb21a4a5fa0b82df1a00ebcdc563fe968
hudeng <hudeng@deepin.org>
2023-06-25 16:22:56 +0800

    obs: docker build scripts optimize
    support inheritance of environment variables for ordinary users

--- services/obs/docker-build.sh
+++ services/obs/docker-build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo docker build -t hub.deepin.com/k3s/obs/api -f api/Dockerfile .
-sudo docker build -t hub.deepin.com/k3s/obs/src -f src/Dockerfile .
-sudo docker build -t hub.deepin.com/k3s/obs/backend -f backend/Dockerfile .
-sudo docker build -t hub.deepin.com/k3s/obs/scheduler -f scheduler/Dockerfile .
-sudo docker build -t hub.deepin.com/k3s/obs/worker -f worker/Dockerfile .
-sudo docker push hub.deepin.com/k3s/obs/api
-sudo docker push hub.deepin.com/k3s/obs/src
-sudo docker push hub.deepin.com/k3s/obs/backend
-sudo docker push hub.deepin.com/k3s/obs/scheduler
-sudo docker push hub.deepin.com/k3s/obs/worker
+sudo -E docker build -t hub.deepin.com/k3s/obs/api -f api/Dockerfile .
+sudo -E docker build -t hub.deepin.com/k3s/obs/src -f src/Dockerfile .
+sudo -E docker build -t hub.deepin.com/k3s/obs/backend -f backend/Dockerfile .
+sudo -E docker build -t hub.deepin.com/k3s/obs/scheduler -f scheduler/Dockerfile .
+sudo -E docker build -t hub.deepin.com/k3s/obs/worker -f worker/Dockerfile .
+sudo -E docker push hub.deepin.com/k3s/obs/api
+sudo -E docker push hub.deepin.com/k3s/obs/src
+sudo -E docker push hub.deepin.com/k3s/obs/backend
+sudo -E docker push hub.deepin.com/k3s/obs/scheduler
+sudo -E docker push hub.deepin.com/k3s/obs/worker
