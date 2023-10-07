bb9afe59421b69feaf3184c5d9b73e79bb97e403
hudeng <hudeng@deepin.org>
2023-10-07 22:36:32 +0800

    obs: Update backend image
    1. 优化服务启动脚本,添加服务重试机制
    2. 去除基于iptables的端口转发
    
    log:

--- services/obs/backend/entrypoint.sh
+++ services/obs/backend/entrypoint.sh
@@ -8,20 +8,29 @@ do
     wg-quick up $wg
 done
 
-# Add port forwarding
-echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -A PREROUTING -p tcp --dport $SRCPORT -j DNAT --to-destination $SRCIP:$SRCPORT
-iptables -t nat -A POSTROUTING -d $SRCIP -p tcp --dport $SRCPORT -j SNAT --to-source $BACKENDIP
-
 # Start obs backend services
-/usr/lib/obs/server/bs_repserver --logfile rep_server.log &
-/usr/lib/obs/server/bs_dispatch --logfile dispatcher.log &
-/usr/lib/obs/server/bs_publish --logfile publisher.log &
-/usr/lib/obs/server/bs_warden --logfile warden.log &
-/usr/lib/obs/server/bs_dodup --logfile dodup.log &
-/usr/lib/obs/server/bs_getbinariesproxy --logfile getbinariesproxy.log &
-/usr/sbin/obsscheduler start
-/usr/lib/obs/server/bs_notifyforward --logfile notifyforward.log
+starting() {
+    /usr/lib/obs/server/bs_repserver --logfile rep_server.log &
+    /usr/lib/obs/server/bs_dispatch --logfile dispatcher.log &
+    /usr/lib/obs/server/bs_publish --logfile publisher.log &
+    /usr/lib/obs/server/bs_warden --logfile warden.log &
+    /usr/lib/obs/server/bs_dodup --logfile dodup.log &
+    /usr/lib/obs/server/bs_getbinariesproxy --logfile getbinariesproxy.log &
+    /usr/sbin/obsscheduler start
+    /usr/lib/obs/server/bs_notifyforward --logfile notifyforward.log
+}
 
-# For debug
-while true;do sleep 6;done
+stop() {
+    ps -ef |grep bs_ |awk '{print $2}' |xargs kill -9 || true
+    rm -f /srv/obs/run/*.lock
+    # Clean down workers
+    rm -f /srv/obs/workers/down/*
+}
+
+# For auto restart
+while true
+do
+    starting
+    stop
+    sleep 6
+done
