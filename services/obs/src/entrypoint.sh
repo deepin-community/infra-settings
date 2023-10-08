94c74910d2f8560055007803c9f5f4eea2f9c826
hudeng <hudeng@deepin.org>
2023-10-08 11:44:58 +0800

    obs: src service image update
    优化src服务启动脚本

--- services/obs/src/entrypoint.sh
+++ services/obs/src/entrypoint.sh
@@ -2,12 +2,25 @@
 set -x
 
 cp /srv/obs/_configuration.xml /srv/obs/configuration.xml
-/usr/lib/obs/server/bs_redis --logfile log/redis.log &
-/usr/lib/obs/server/bs_deltastore --logfile deltastore.log &
-/usr/lib/obs/server/bs_service --logfile log/src_service.log &
-/usr/lib/obs/server/bs_servicedispatch --logfile log/servicedispatch.log &
-/usr/lib/obs/server/bs_srcserver --logfile log/src_server.log &
-/usr/lib/obs/server/bs_sourcepublish --logfile log/sourcepublish.log
 
-# For debug
-while true;do sleep 6;done
+starting() {
+    /usr/lib/obs/server/bs_redis --logfile log/redis.log &
+    /usr/lib/obs/server/bs_deltastore --logfile deltastore.log &
+    /usr/lib/obs/server/bs_service --logfile log/src_service.log &
+    /usr/lib/obs/server/bs_servicedispatch --logfile log/servicedispatch.log &
+    /usr/lib/obs/server/bs_srcserver --logfile log/src_server.log &
+    /usr/lib/obs/server/bs_sourcepublish --logfile log/sourcepublish.log
+}
+
+stop() {
+    ps -ef |grep bs_ |awk '{print $2}' |xargs kill -9 || true
+    rm -f /srv/obs/run/*.lock
+}
+
+# For auto restart
+while true
+do
+    starting
+    stop
+    sleep 6
+done
