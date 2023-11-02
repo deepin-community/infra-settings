a0ec81b079056c5d98b3c3c33a6e5e3666153511
hudeng <hudeng@deepin.org>
2023-11-02 21:49:31 +0800

    obs: worker service image update
    add 'tar' for obs ccache

--- services/obs/worker/entrypoint.sh
+++ services/obs/worker/entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+
+# 修改hostname,使其包含obs 节点的名称
+OLDNAME=$(hostname)
+HOSTSUFFIX=$(hostname |awk -F '-' '{print $3"-"$5"-"$6}')
+export HOSTNAME="$NODENAME-$HOSTSUFFIX"
+echo "$HOSTNAME" > /proc/sys/kernel/hostname
+echo "$HOSTNAME" > /etc/hostname
+sed "s/$OLDNAME/$HOSTNAME/g" /etc/hosts >> /etc/hosts
+echo "Worker hostname: $HOSTNAME"
 /usr/sbin/obsworker start
 while true;do sleep 6;done
