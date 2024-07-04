e1f47888689c046b01271ac64cff14c657d44ce1
hudeng <hudeng@deepin.org>
2024-07-04 16:16:51 +0800

    obs: src service add github ssh clone support

--- services/obs/src/entrypoint.sh
+++ services/obs/src/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -x
 
+# prepare ssh key for git ssh clone
+mkdir -p /home/obsservicerun/.ssh
+chown obsservicerun:obsservicerun -R /home/obsservicerun
+chmod 700 -R /home/obsservicerun
+usermod -d /home/obsservicerun obsservicerun
+cp /home/obs_ed25519 /home/obsservicerun/.ssh/id_ed25519 && chmod 400 /home/obsservicerun/.ssh/id_ed25519 || true
+cp /home/known_hosts /home/obsservicerun/.ssh/known_hosts && chmod 600 /home/obsservicerun/.ssh/known_hosts || true
+chown obsservicerun:obsservicerun -R /home/obsservicerun
+
 cp /srv/obs/_configuration.xml /srv/obs/configuration.xml
 
 starting() {
